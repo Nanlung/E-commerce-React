fce118f798f9367f17f833b19f7ff6284a78a082
Nanlung <nanlung.temlong@gmail.com>
2021-04-28 23:40:35 +0100

    counter comp fix

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,34 +2,34 @@ import React, { Component } from 'react';
 
 
 class Counter extends Component {
-  
- 
-
-  
 
 
-  render() { 
-    
+
+
+
+
+  render() {
+
     return (<div>
       {this.props.children}
       <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
       <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
       <button onClick={this.props.handleDecrement} className="btn btn-secondary m-2 btn-sm">Decrement</button>
-      
-      </div>);
+
+    </div>);
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.props.counter.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const {count} = this.props.counter;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
   }
 
 }
- 
+
 export default Counter;
